a44700d72b1db5146c67409e773869231bb8259f
opussf <github@zz9-za.com>
2026-08-04 12:51:30 -0700

    XML Frame, and initial src

---
+++ src/Tracker.lua
@@ -0,0 +1,7 @@
+TRACKER_SLUG, TRACKER = ...
+TRACKER_MSG_ADDONNAME = C_AddOns.GetAddOnMetadata( TRACKER_SLUG, "Title" )
+TRACKER_MSG_VERSION   = C_AddOns.GetAddOnMetadata( TRACKER_SLUG, "Version" )
+TRACKER_MSG_AUTHOR    = C_AddOns.GetAddOnMetadata( TRACKER_SLUG, "Author" )
+
+TRACKER_data = {}
+
